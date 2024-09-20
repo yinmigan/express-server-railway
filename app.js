6d0d9a797981e7f374a51f6497c546877aca3621
Maria Michelle Eballena <maria.eballena@lanexcorp.com>
2024-09-20 16:26:24 +0800

    updated port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const pool = require('./db'); // Import the pool
 const app = express();
-const port = 3200;  // You can use any port number you prefer
+const port = 8080;  // You can use any port number you prefer
 
 // Middleware to parse JSON bodies
 app.use(express.json());
